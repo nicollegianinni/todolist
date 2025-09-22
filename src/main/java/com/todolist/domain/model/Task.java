fc3755223f102635785e4472c8055fe9f4e96fa2
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-09-22 20:21:46 -0300

    camada de domain done

--- src/main/java/com/todolist/domain/model/Task.java
+++ src/main/java/com/todolist/domain/model/Task.java
@@ -1,4 +1,71 @@
 package com.todolist.domain.model;
 //model entidade na tabela
 public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private String dueDate;
+
+    public Task(Long id, String title, String description, String status, String dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                ", dueDate='" + dueDate + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/todolist/domain/port/TaskRepositoryPort.java
+++ src/main/java/com/todolist/domain/port/TaskRepositoryPort.java
@@ -1,4 +1,14 @@
 package com.todolist.domain.port;
+
+import com.todolist.domain.model.Task;
+
+import java.util.List;
+import java.util.Optional;
+
 //portas (interface)
 public interface TaskRepositoryPort {
+    Task save(Task task);
+    List<Task> findAll();
+    Optional<Task> findById(Long id);
+    void deleteById(Long id);
 }
